c13150bd69f5f8a421b5378ae5dc184d2cad9412
Rissochek <ruslanynysov@mail.ru>
2025-09-25 14:14:43 +0300

    feat: added tag version in index

--- internal/server/index.go
+++ internal/server/index.go
@@ -1,13 +1,15 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func (s *httpServer) handleIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	response := "Hello, world!"
+	tag := "v0.1.0"
+	response := fmt.Sprintf("Hello, world! %v", tag)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.Write([]byte(response))
 }
